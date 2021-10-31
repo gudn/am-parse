4563c35459d37670f5ddd97c2c71b5f1a499755b
Daniil Novoselov <gudnmail@gmail.com>
2021-10-31 17:24:08 +0500

    docs: add simple doc to convert func

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,12 @@ pub enum OutputFormat {
   Latex,
 }
 
+/// Converts input string to specified format
+/// ```
+/// use am_parse::{convert, OutputFormat};
+/// assert_eq!(convert("1+1".into(), OutputFormat::Latex, vec![]), "1+1");
+/// assert_eq!(convert("fx = 1/2".into(), OutputFormat::Latex, vec!["f"]), "f{\\left(x\\right)}=\\frac{1}{2}");
+/// ```
 pub fn convert(
   input: String,
   output_format: OutputFormat,
