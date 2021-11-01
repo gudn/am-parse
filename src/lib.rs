1aa4ea9ce5861f0e3906cf15ecf99cb66b34608d
Daniil Novoselov <gudnmail@gmail.com>
2021-11-01 23:44:14 +0500

    add wasm support

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+use wasm_bindgen::prelude::*;
+
 mod parse;
 mod render;
 mod tokens;
 mod trie;
 
+#[wasm_bindgen]
 pub enum OutputFormat {
   Latex,
 }
@@ -13,14 +16,20 @@ pub enum OutputFormat {
 /// assert_eq!(convert("1+1".into(), OutputFormat::Latex, vec![]), "1+1");
 /// assert_eq!(convert("fx = 1/2".into(), OutputFormat::Latex, vec!["f"]), "f{\\left(x\\right)}=\\frac{1}{2}");
 /// ```
-pub fn convert(
-  input: String,
-  output_format: OutputFormat,
-  custom_functions: Vec<&str>,
-) -> String {
+pub fn convert(input: String, output_format: OutputFormat, custom_functions: Vec<&str>) -> String {
   let tokens = tokens::tokenize(&input, custom_functions);
   let expr = parse::parse(tokens);
   render::render(expr, output_format)
+}
+
+#[wasm_bindgen]
+pub fn convert_(input: String, output_format: OutputFormat, custom_functions: String) -> String {
+  let cf = if custom_functions.is_empty() {
+    Vec::new()
+  } else {
+    custom_functions.split(' ').collect()
+  };
+  convert(input, output_format, cf)
 }
 
 #[cfg(test)]
